50167e14498f7c6b70a4ac869711297f3b27ffc7
SHIVAM KUMAR <shivamsormar123@gmail.com>
2026-07-24 10:31:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict" //treats all JS code as newer version
+
+//  alert(3+3) //we are using node JS not browser
+
+console.log("SHIVAM");
+
+let name  = "Shivam"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => Unique
+
+// object
+
+console.log(typeof undefined); // Undefined
+console.log(typeof null);   // Object
+
+
