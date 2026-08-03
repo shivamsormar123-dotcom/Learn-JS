942d464deaa8ddcad0f8d74b71913c04f370dda2
SHIVAM KUMAR <shivamsormar123@gmail.com>
2026-08-03 17:06:38 +0000

    Datatype Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+const Score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const isOutsideTemp = null
+let userEmail;
+
+
+const ID = Symbol('1234')
+const anotherId = Symbol('123')
+
+// console.log(ID ===anotherId);
+
+const bigNumber = 13234656789455n;
+// console.log(typeof bigNumber)
+
+// Reference(Non-Primitive)
+// Array, Objects, Functions 
+
+const Names = ["SHIVAM", "SURAJ", "RITESH"]
+
+// let myObject = {
+//     name: = "SHIVAM",
+//     age: = 20,
+
+// }
+
+const myFunction = function(){
+    console.log("Hello SHIVAM");
+}
+
+console.log(typeof anotherId)
