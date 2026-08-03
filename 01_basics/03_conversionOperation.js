d8dc25ad0580139f505826f25bd854bb2d42e4d6
SHIVAM KUMAR <shivamsormar123@gmail.com>
2026-08-03 09:30:25 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let Score = "40Ab"
+
+
+// console.log(typeof(Score));
+// console.log(typeof Score);
+
+let ValueInNumber = Number(Score)
+// console.log(typeof(ValueInNumber));
+// console.log(ValueInNumber);
+
+//Note:
+// "33" =>33
+// "40Ab" => NaN (Not an Number)
+// True => 1; // False => 0
+
+let IsLoggedIn = ""
+ let booleanIsLoggedIn = Boolean(IsLoggedIn)
+ 
+//  console.log(booleanIsLoggedIn);
+
+ // 1 => True; // 0 => False
+ // "" => False; //"SHIVAM" => True
+
+
+ let someNumber = 40
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
+ 
+ 
+ 
+
+
+
+
